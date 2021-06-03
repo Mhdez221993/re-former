26f24bdb48d0f109c4117143ade072352c0b2959
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-03 14:31:06 +0200

    Fix rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,9 @@
 module UsersHelper
-  def error_helper(_user)
-    if @user.errors.any?
-      pluralize(@user.errors.count, 'error')
+  def error_helper(user)
+    if user.errors.any?
+      pluralize(user.errors.count, 'error')
       err = ''
-      @user.errors.full_messages.each do |msg|
-        err << "#{msg}  "
-      end
+      @user.errors.full_messages.each { |msg| err << "#{msg}  " }
       err
     end
   end
