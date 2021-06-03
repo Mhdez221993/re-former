1277dd8ec0f3a5f89fe984586c70866cfce1710d
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-03 14:41:02 +0200

    Fix rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,12 @@
+# rubocop: disable Style/GuardClause
 module UsersHelper
   def error_helper(user)
+    err = ''
     if user.errors.any?
-      pluralize(user.errors.count, 'error')
-      err = ''
       @user.errors.full_messages.each { |msg| err << "#{msg}  " }
       err
     end
   end
 end
+
+# rubocop: enable Style/GuardClause
