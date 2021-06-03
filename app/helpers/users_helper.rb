69e95e9e84ab9b25bce7fc3c33d59bc9b5cfd95c
ZeenatLawal <zeenatlawal82@gmail.com>
2021-06-03 09:05:57 +0100

    move logic to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def error_helper(user)
+    if @user.errors.any?
+      pluralize(@user.errors.count, "error")
+      @user.errors.full_messages.each do |msg|
+        msg
+      end
+    end
+  end
 end
